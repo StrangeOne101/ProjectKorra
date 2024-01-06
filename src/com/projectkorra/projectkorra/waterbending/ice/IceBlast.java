cec860d12db6afe554e1186fb7bbe46c19cb4feb
StrangeOne101 <strange.toby@gmail.com>
2024-01-07 12:22:18 +1300

    Beta 3 changes

--- src/com/projectkorra/projectkorra/waterbending/ice/IceBlast.java
+++ src/com/projectkorra/projectkorra/waterbending/ice/IceBlast.java
@@ -244,6 +244,10 @@ public class IceBlast extends IceAbility {
 			this.remove();
 			return;
 		}
+		if (this.prepared && !isIce(this.sourceBlock)) {
+			this.remove();
+			return;
+		}
 
 		if (System.currentTimeMillis() < this.time + this.interval) {
 			return;
